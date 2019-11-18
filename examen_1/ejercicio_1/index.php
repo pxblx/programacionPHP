3a70c71acceb4a30ed5003d19365f9bbcce8a7f8
pxblx <pablomurilloavila@gmail.com>
2019-11-18 20:05:19 +0100

    Ejercicio 1 del examen (con CSS)

--- examen_1/ejercicio_1/index.php
+++ examen_1/ejercicio_1/index.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Examen de la unidad 1
+ * Ejercicio 1
  * 
  * @author Pablo
  */
@@ -18,25 +19,25 @@ $datos = array (
                                         array ("dia" => "Viernes", "hora" => "3ª"),
                                         array ("dia" => "Viernes", "hora" => "4ª"))),
     "DWEC" => array ("nombre" => "Desarrollo Web en Entorno Cliente",
-                    "profesor" => "Lourdes",
+                    "profesor" => "Lourdes Magarín",
                     "horario" => array (array ("dia" => "Lunes", "hora" => "1ª"),
                                         array ("dia" => "Lunes", "hora" => "2ª"),
                                         array ("dia" => "Miércoles", "hora" => "4ª"),
                                         array ("dia" => "Miércoles", "hora" => "5ª"),
                                         array ("dia" => "Viernes", "hora" => "5ª"),
                                         array ("dia" => "Viernes", "hora" => "6ª"))),
-    "HLC" => array ("nombre" => "HLC Android",
+    "HLC" => array ("nombre" => "Android",
                     "profesor" => "José Aguilera",
                     "horario" => array (array ("dia" => "Martes", "hora" => "3ª"),
                                         array ("dia" => "Martes", "hora" => "4ª"),
                                         array ("dia" => "Miércoles", "hora" => "6ª"))),
     "DAW" => array ("nombre" => "Despliegue de Aplicaciones Web",
-                    "profesor" => "Mª Carmen",
+                    "profesor" => "Mª Carmen Tripiana",
                     "horario" => array (array ("dia" => "Miércoles", "hora" => "3ª"),
                                         array ("dia" => "Jueves", "hora" => "5ª"),
                                         array ("dia" => "Jueves", "hora" => "6ª"))),
     "DIW" => array ("nombre" => "Diseño de Interfaces Web",
-                    "profesor" => "Jaime",
+                    "profesor" => "Jaime Rabasco",
                     "horario" => array (array ("dia" => "Lunes", "hora" => "5ª"),
                                         array ("dia" => "Lunes", "hora" => "6ª"),
                                         array ("dia" => "Jueves", "hora" => "1ª"),
@@ -44,7 +45,7 @@ $datos = array (
                                         array ("dia" => "Martes", "hora" => "1ª"),
                                         array ("dia" => "Martes", "hora" => "2ª"))),
     "EIE" => array ("nombre" => "Empresa e Iniciativa Emprendedora",
-                    "profesor" => "Gema",
+                    "profesor" => "Mª Gema Sánchez",
                     "horario" => array (array ("dia" => "Miércoles", "hora" => "1ª"),
                                         array ("dia" => "Miércoles", "hora" => "2ª"),
                                         array ("dia" => "Viernes", "hora" => "1ª"),
@@ -93,29 +94,6 @@ function dia_semana_nombre($dia_semana) {
             return "Viernes";
     }
 }
-
-/**
- * Devolver el color de fondo según el módulo
- * 
- * @param modulo Módulo
- * @return string Color HTML
- */
-function modulo_color($modulo) {
-    switch ($modulo) {
-        case "DWEC":
-            return "ffabab";
-        case "DWES":
-            return "bffcc6";
-        case "DAW":
-            return "fff5ba";
-        case "DIW":
-            return "afcbff";
-        case "HLC":
-            return "e7ffac";
-        case "EIE":
-            return "d5aaff";
-    }
-}
 ?>
 
 <!DOCTYPE html>
@@ -125,23 +103,24 @@ function modulo_color($modulo) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="author" content="Pablo">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Horario 2º DAW 2019/2020</title>
 </head>
 <body>
-    <h1>Horario 2º DAW 2019/2020</h1><hr/><br/>
+    <h1>Horario 2º DAW 2019/2020</h1><hr/><br/><div id="tablas">
     <?php
     // Mostrar la tabla del horario
-    echo "<table border=\"1\" cellpadding=\"10\">";
+    echo "<table id=\"horario\">";
     echo "<tr>";
     for ($c = 1; $c <= 5; $c++) {
-        echo "<td align=\"center\" width=\"15%\" bgcolor=\"cccccc\"><b>".dia_semana_nombre($c)."</b></td>";
+        echo "<td class=\"titulo\"><b>".dia_semana_nombre($c)."</b></td>";
     }
     echo "</tr>";
     for ($f = 1; $f <= 6; $f++) {
         echo "<tr>";
         for ($c = 1; $c <= 5; $c++) {
             $modulo = modulo_dia_hora($datos, dia_semana_nombre($c), ($f."ª"));
-            echo "<td align=\"center\" width=\"15%\" bgcolor=\"".modulo_color($modulo)."\">".$modulo."</td>";
+            echo "<td class=\"".$modulo."\">".$modulo."</td>";
         }
         echo "</tr>";
     }
@@ -150,12 +129,13 @@ function modulo_color($modulo) {
     <br/>
     <?php
     // Mostrar la leyenda
-    echo "<table border=\"1\" cellpadding=\"10\">";
-    echo "<tr><td colspan=\"2\" align=\"center\" bgcolor=\"cccccc\"><b>Leyenda</b></td></tr>";
+    echo "<table id=\"leyenda\">";
+    echo "<tr><td class=\"titulo\" colspan=\"2\"><b>Leyenda</b></td></tr>";
     foreach ($datos as $k1 => $v1) {
-        echo "<tr><td align=\"center\" width=\"80%\">".$datos[$k1]["nombre"]." - ".$datos[$k1]["profesor"]."</td><td width=\"80%\" bgcolor=\"".modulo_color($k1)."\"></td></tr>";
+        echo "<tr><td>".$datos[$k1]["nombre"]." — ".$datos[$k1]["profesor"]."</td><td class=\"".$k1."\">".$k1."</td></tr>";
     }
     echo "</table>";
     ?>
+    </div>
 </body>
 </html>
